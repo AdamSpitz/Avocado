5c0162036e82fbc5e628f77bd35644628c4209f6
Adam Spitz <adam.spitz@gmail.com>
2011-11-08 08:59:55 -0800

    Added JSONP functionality.

--- public/javascripts/core/http.js
+++ public/javascripts/core/http.js
@@ -19,6 +19,30 @@ thisModule.addSlots(avocado.http, function(add) {
     return paramsMir.normalSlots().map(function(s) { return encodeURIComponent(s.name()) + "=" + encodeURIComponent(s.contents().reflectee()); }).toArray().join("&");
   }, {category: ['requests']});
   
+  add.data('jsonpCallbacks', {}, {category: ['JSONP'], initializeTo: '{}'});
+
+  add.method('findUnusedNameForJSONP', function () {
+    var i = 0;
+    var jsonp;
+    var callbackHolder = this.jsonpCallbacks;
+    while (true) {
+      jsonp = "jsonpCallback_" + (++i);
+      if (! callbackHolder.hasOwnProperty(jsonp)) { return jsonp; }
+    }
+  }, {category: ['JSONP']});
+  
+  add.method('jsonpGet', function (path, callback) {
+    var callbackName = this.findUnusedNameForJSONP();
+    var jsonp = "avocado.http.jsonpCallbacks." + callbackName;
+    avocado.http.jsonpCallbacks[callbackName] = function(json) { delete avocado.http.jsonpCallbacks[callbackName]; callback(json); }
+    
+    if (path.include('?')) { path += '&jsonp=' + jsonp; } else { path += '?jsonp=' + jsonp; }
+    
+    var script = document.createElement('script');
+    script.setAttribute('src', path);
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }, {category: ['JSONP']});
+  
 });
 
 
